1d4abc9b82d807d23fe662feac51b2847d0c72e6
Bushra Rahman <127424846+BushraR1709@users.noreply.github.com>
2026-07-10 03:55:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+
+import streamlit as st
+from google import genai
+
+# Create Gemini client
+client = genai.Client(api_key="AQ.Ab8RN6K3bPZOGQCL5FeW5bwTZzx7qlQ2ICWC_RbLh9JSx_jm1w")
+
+# Page configuration
+st.set_page_config(
+    page_title="AI Learning Buddy",
+    page_icon="🎓",
+    layout="centered"
+)
+
+# Title
+st.title("🎓 AI Learning Buddy")
+
+st.write("Learn any topic with your AI Learning Buddy!")
+
+# User Input
+topic = st.text_input("Enter a Topic")
+
+# Activity Selection
+option = st.selectbox(
+    "Choose Activity",
+    [
+        "Explain Concept",
+        "Real-Life Example",
+        "Generate Quiz",
+        "Ask Anything"
+    ]
+)
+
+# Generate Button
+if st.button("Generate"):
+
+    if topic.strip() == "":
+        st.warning("Please enter a topic.")
+
+    else:
+
+        if option == "Explain Concept":
+            prompt = f"""
+            Explain {topic} in simple language for a beginner.
+            Use simple words and step-by-step explanation.
+            """
+
+        elif option == "Real-Life Example":
+            prompt = f"""
+            Give one simple real-life example of {topic}.
+            """
+
+        elif option == "Generate Quiz":
+            prompt = f"""
+            Create 5 multiple choice questions on {topic}.
+
+            For each question provide:
+            - Four options (A, B, C, D)
+            - Correct Answer
+            """
+
+        else:
+            prompt = topic
+
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=prompt
+            )
+
+            st.success("Response Generated Successfully!")
+            st.write(response.text)
+
+        except Exception as e:
+            st.error(f"Error: {e}")
